6ab4cddcf5835e355f2b94cca6cd9e98e5516c67
Chris Wood <104919783+ChrisXHL@users.noreply.github.com>
2023-11-06 23:37:29 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,11 @@
+import openai
+
+# 初始化OpenAI API
+openai.api_key = ""
+
+def optimize_prompt(initial_prompt, optimization_times=5):
+    # 省略了实现细节，假定这里是原始脚本的内容
+    pass
+
+initial_prompt = input("请输入原文：")
+optimized_prompt = optimize_prompt(initial_prompt, optimization_times=5)
